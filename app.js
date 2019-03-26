24046d813964d3634fe8707f412b868fb19d9eec
tisuchi <tisuchi@gmail.com>
2019-03-26 11:36:45 +0600

    Add global function file and directory name

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-var time = 0;
+// var time = 0;
+//
+// var timer = setInterval(function(){
+//     time+= 2;
+//     console.log(time + " has passed.");
+//
+//     if (time > 5) {
+//         clearInterval(timer);
+//     }
+// }, 2000)
 
-var timer = setInterval(function(){
-    time+= 2;
-    console.log(time + " has passed.");
 
-    if (time > 5) {
-        clearInterval(timer);
-    }
-}, 2000)
+// Get file and diretory name
+console.log(__dirname);
+console.log(__filename);
